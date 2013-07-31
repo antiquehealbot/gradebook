2ec1a7a78f430bb0d37491192a9efd035ea435a1
owenlyn <wou6@gatech.edu>
2013-07-31 12:58:02 -0400

    javadoc

--- src/main/java/gradebook/model/GradingScheme.java
+++ src/main/java/gradebook/model/GradingScheme.java
@@ -3,11 +3,11 @@ package gradebook.model;
 /**
   * Representing a GradingScheme object, leaving as an interface
   * open for extension and modification for users.
-  * 
+  *
   * This class implements the "Abstract Factory" Design Pattern.
   * This class provieds an interface for creating a family of
-  * GradingScheme objects with diffreent ways to calculate 
-  * scores. 
+  * GradingScheme objects with diffreent ways to calculate
+  * scores.
   */
 
 public interface GradingScheme {
